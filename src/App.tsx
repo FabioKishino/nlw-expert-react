c0e160a5862c8b4d7f33d4c3be5227314c6b7e2f
fabiokishino <fabiokishino@hotmail.com>
2024-03-24 16:04:21 -0300

    Aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,10 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Aqui vai o conteúdo da nota'
+        }} />
       </div>
     </div>
   )
